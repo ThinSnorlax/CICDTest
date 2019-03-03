771e36fa0df6f8865eb5f2c15149ca8782cf8bf4
snorlax <574937363@11.com>
2019-03-03 21:26:00 +0800

    new add

--- src/main/java/snor/test/Hello.java
+++ src/main/java/snor/test/Hello.java
@@ -7,6 +7,7 @@ import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
+import java.net.InetAddress;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -24,6 +25,27 @@ public class Hello {
             return Response.status(Response.Status.OK).entity(json.toJSONString(table_names)).build();
         }
         return Response.status(Response.Status.OK).entity(json.toJSONString(table_names)).build();
+    }
+
+    @Path("getServerIP")
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    public Response getServerIP() {
+        List<String> IPInfo = new ArrayList<>();
+        JSONObject json = new JSONObject();
+        InetAddress ia=null;
+        try {
+            ia=ia.getLocalHost();
+
+            String localname=ia.getHostName();
+            String localip=ia.getHostAddress();
+            IPInfo.add("本机名称是："+ localname);
+            IPInfo.add("本机的ip是 ："+localip);
+        } catch (Exception e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+        return Response.status(Response.Status.OK).entity(json.toJSONString(IPInfo)).build();
     }
 
 }
