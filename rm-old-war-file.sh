70a549a9399966578fd7a09108160623e631c480
snorlax <574937363@11.com>
2019-03-04 21:52:20 +0800

    change shell

--- rm-old-war-file.sh
+++ rm-old-war-file.sh
@@ -1,3 +1,7 @@
 #!/bin/bash
 # 删除旧的包
-sudo rm /usr/local/tomcat/apache-tomcat-9.0.16/webapps/snor-test.war
+if rm /usr/local/tomcat/apache-tomcat-9.0.16/webapps/snor-test.war ;then
+    echo "Successfully" 1>&2
+else
+    echo "Failed" 1>&2
+fi
